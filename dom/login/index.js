bd866bfb0088d72e00ba377e4e2a8ed8a4ee5fe4
nick bui <tronggiao1908@gmail.com>
2024-05-22 11:51:11 +0200

    add dom model follow along

---
+++ dom/login/index.js
@@ -0,0 +1,19 @@
+const onSubmit = () => {
+    const registerForm = document.getElementById("registerForm");
+
+    if (format.checkValidity()){
+        form.classList.add('was-validated');
+        
+        const firstName = document.getElementById("firstName").value;
+        const lastName = document.getElementById("lastName").value;
+        const email = document.getElementById("email").value;
+        const password = document.getElementById("password").value;
+        const bio = document.getElementById("bio").value;
+
+        console.log(firstName, lastName, password, email, bio);
+    } else {
+        console.log("Form not valid");
+    }
+
+}
+
