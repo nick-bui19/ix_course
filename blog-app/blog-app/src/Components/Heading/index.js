38ef2014fe5155f8bae3880d4eacddec5181f474
nick bui <tronggiao1908@gmail.com>
2024-05-28 11:39:43 +0200

    removed blog-app (1) folder and its content

--- blog-app/blog-app/src/Components/Heading/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import './index.css';
-
-export default function Heading() {
-  return <p className="page-title">THE BLOG</p>;
-}
--- blog-app/blog-app/src/Components/SubHeading/index.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import './index.css';
-
-export default function SubHeading({ subHeading }) {
-  return <p className="page-subtitle d-flex w-100 justify-content-left align-items-left">{subHeading}</p>;
-}
